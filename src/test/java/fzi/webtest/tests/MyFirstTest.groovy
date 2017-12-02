74ecdee6d2fc0f6cd02d1015d5c9a132de1d4bf4
Fezi <fezi@users.noreply.github.com>
2017-12-02 22:54:15 +0100

    minor

--- src/test/java/fzi/webtest/tests/MyFirstTest.groovy
+++ src/test/java/fzi/webtest/tests/MyFirstTest.groovy
@@ -8,9 +8,9 @@ import org.testng.annotations.Test
 class MyFirstTest implements GebReportingTestTrait {
 
     @Test
-    void firstTest() {
+    void visistGebDocumentationAndClickSidebarLink() {
         MyFirstPage page = browser.to MyFirstPage
-        println page.headline.text()
+        println page.headline.text() // "The Book Of Geb"
         assert page.headline.text().contains('Geb')
 
         page.sidebar.sidemenuAnchors.getAt(2).click()
